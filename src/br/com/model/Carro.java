39f5db610eeef5066ff797c66eaffdbdd64bfe4b
Evelyn Juliane <evelynbr@live.com>
2020-01-10 11:40:59 -0300

    limpo

---
+++ src/br/com/model/Carro.java
@@ -0,0 +1,12 @@
+package br.com.model;
+
+public class Carro extends Veiculo {
+
+		private String marca;
+		private String modelo;
+		private String placa;
+		private String cor;
+		private float Km;
+		private boolean isLigado;
+		
+}
